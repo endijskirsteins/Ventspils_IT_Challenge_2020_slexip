9aa5906f74ee3f7d0fc7d69b72e64165da5db5e8
Zappingmadnnes <roberts.mezals@gmail.com>
2020-10-14 17:16:28 +0300

    Lake pro

--- VITC Water Contamination/Assets/Scripts/Lake.cs
+++ VITC Water Contamination/Assets/Scripts/Lake.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Lake : MonoBehaviour
 {
+    SpriteRenderer sprite;
+    public float contamination = 0.0f;
+    public float water = 10000.0f;
+    public Vector4 Start_Color;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Start_Color = new Vector4(0.0f, 1.0f, 0.0f, 1.0f);
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.color = Start_Color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        tra
+        //sprite.color = new Color(contamination / water, 0.7172986f, 0.8490566f, 1);
     }
 }
